fe966a9d0930c9e0367bdc51a70661a4913daf43
Andrey999 <1985gromovandrey@gmail.com>
2019-03-08 20:54:30 +0300

    add feedback form and scripts

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// burger  menu
 let menu = document.getElementById('header__menu-burger');
 let showMenu = document.getElementById('mobile-menu');
 menu.addEventListener('click', openMenu);
@@ -11,3 +12,35 @@ function openMenu() {
     span2.classList.toggle('line-2-hidden');
     span3.classList.toggle('line-3');
 }
+// burger  menu  end
+
+
+//   add form
+let contactUs = document.getElementById('main__offer-btn');
+contactUs.addEventListener('click', addModal);
+
+function addModal() {
+    let modal = document.getElementById('modal');
+    let modalFeedback = document.getElementById('form__block-feedback');
+    let btnSend = document.getElementById('form__block-btn');
+    let modalClose = document.getElementById('form__block-close');
+    modal.style.display = 'block';  // onclick add form
+
+    modalClose.onclick = function() {  //  onclick to X hide form
+        modal.style.display = 'none';
+    };
+
+    btnSend.onclick = function() {  // onclick on btn form
+        if((document.getElementById('name').value == '') || (document.getElementById('email').value == '') || (document.getElementById('tel').value == '')) {
+            modalFeedback.style.display = 'none';
+        }
+        else {
+            modalFeedback.style.display = 'block';  // show  feedback  window
+            setTimeout(()=>{
+                modal.style.display = 'none';  //  hide window form
+                modalFeedback.style.display = 'none';  //   hide feedback form
+            },2000);
+        }
+    };
+}
+
